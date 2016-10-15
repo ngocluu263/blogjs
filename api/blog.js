01b80b86fc5f29ace4c7578dc4151e500f31ce24
Luu Nguyen <luunguyen@ccintegration.com>
2016-10-16 04:45:41 +0700

    Update

--- api/blog.js
+++ api/blog.js
@@ -18,7 +18,7 @@ routes.rss = require('./route/rss.js');
 
 
 app.all('*', function(req, res, next) {
-  res.set('Access-Control-Allow-Origin', 'http://192.168.1.150');
+  res.set('Access-Control-Allow-Origin', 'http://localhost');
   res.set('Access-Control-Allow-Credentials', true);
   res.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
   res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
