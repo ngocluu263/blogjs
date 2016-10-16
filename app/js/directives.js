0fda1d38675dc08d49a86d70f1dd7f2a08673e2a
Luu Nguyen <luunguyen@ccintegration.com>
2016-10-16 07:07:20 +0700

    Update

--- app/js/directives.js
+++ app/js/directives.js
@@ -8,9 +8,9 @@ appDirectives.directive('displayMessage', function() {
 		template: '<div class="alert {{messageType}}">{{message}}</div>',
 		link: function (scope, element, attributes) {
             scope.$watch(attributes, function (value) {
-            	console.log(attributes);
-            	console.log(value);
-            	console.log(element[0]);
+            	//console.log(attributes);
+            	//console.log(value);
+            	//console.log(element[0]);
                 element[0].children.hide(); 
             });
         }
